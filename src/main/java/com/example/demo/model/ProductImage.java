4e7494618bf700c7929f91b5790299448e79046d
chen-yu-chien <vanilla30362@gmail.com>
2025-07-11 13:26:47 +0800

    移除二手及商品資料表圖片儲存欄位，增加主機儲存上傳圖片功能

--- src/main/java/com/example/demo/model/ProductImage.java
+++ src/main/java/com/example/demo/model/ProductImage.java
@@ -16,11 +16,6 @@ public class ProductImage {
     @ManyToOne(fetch = FetchType.LAZY) //LAZY:懶載入，只有真的要用圖片時才查資料，效能佳;EAGER:立即載入，每次查 product 就會一起查 images
     @JoinColumn(name = "product_id") // 外鍵欄位
     private Product product; //這張圖片所屬的 product 是誰
-    
-    //0614新增
-    @Lob   //表示這是一個「大欄位資料」，讓 JPA 知道這是 byte[] 二進位資料
-    @Column(name = "image_data", columnDefinition = "LONGBLOB")
-    private byte[] imageData; //imageData是真的存圖片內容的欄位，會對應資料表的 image_data 欄位
 
     // Getter & Setter
     public Integer getId() {
@@ -45,13 +40,5 @@ public class ProductImage {
 
     public void setProduct(Product product) {
         this.product = product;
-    }
-    
-    public byte[] getImageData() {
-        return imageData;
-    }
-
-    public void setImageData(byte[] imageData) {
-        this.imageData = imageData;
     }
 }
